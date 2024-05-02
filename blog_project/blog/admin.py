06dec9b8ebd0c92ce49ed5aaca8307da7eb20513
YuzuLiu <s110621063707ste@gmail.com>
2024-05-02 11:04:20 +0800

    Mainly add admin and admin page, and modify some models

--- blog_project/blog/admin.py
+++ blog_project/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Blog, Blogger, Type
+
+# Define the admin class
+class BloggerAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display=('title', 'blogger', 'post_date', 'type')
+    list_filter=('type', 'post_date')
+
 
 # Register your models here.
+admin.site.register(Blogger, BloggerAdmin)
+admin.site.register(Type)
